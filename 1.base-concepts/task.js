af90402a20e1c7a9e83f24077de5e41e6758d00f
Regina <rigifox@mail.ru>
2022-07-21 21:01:36 +0300

    Initial commit 1.base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,50 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  
+  const arr = [];
+  const D = b ** 2 - 4 * a * c;
+
+  if (D < 0) {
+    return arr;
+  } else if (D === 0) {
+    arr.push(-b/(2*a));
+    return arr;
+  } else {
+    arr.push((-b+Math.sqrt(D))/(2*a), (-b-Math.sqrt(D))/(2*a));
+    return arr;
+  }
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  if (isNaN(percent)) {
+    return (`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
+  }
+     
+  if (isNaN(contribution)) {
+    return (`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`);
+  }
+
+  if (isNaN(amount)) {
+    return (`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`);
+  }
+
+  const loanBody = amount - contribution;
+  let months;
+
+  months = (date.getFullYear() - new Date().getFullYear()) * 12;
+  //months -= today.getMonth();
+  //months += date.getMonth();
+  //months <= 0 ? 0 : months;
+
+  const P = percent /(100*12);
+  const monthsPayment = loanBody * (P + (P / (((1 + P) ** months) - 1)));
+
+  totalAmount = Number((months * monthsPayment).toFixed(2));
+  console.log(totalAmount);
 
   return totalAmount;
 }
