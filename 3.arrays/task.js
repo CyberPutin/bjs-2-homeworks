252f24317d60c936d868093d44346260f1c43832
Regina <rigifox@mail.ru>
2022-09-18 19:01:33 +0300

    commit variables 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
+"use strict"
+
+//Задача 1
+
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  return arr1.length === arr2.length && arr1.every((it, i) => it === arr2[i]);
 }
 
+//Задача 2
+
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  return arr.filter(positiveNum => positiveNum > 0 && positiveNum % 3 === 0).map((item) => item * 10);
 }
