f65571bca47b0e1eac841dd06ac945c9ffe2000d
Regina <rigifox@mail.ru>
2022-09-25 02:15:52 +0300

    commit variables 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,42 @@
-function Student(name, gender, age) {
-    // Ваш код
+"use strict"
 
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+const student1 = new Student("Alex", "male", 25);
+const student2 = new Student("Jack", "male", 21);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+  } 
+  else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...rest) {
+  if(this.marks === undefined){ 
+    this.marks = [...rest];
+  } 
+  else {
+    this.marks.push(...rest);
+  }
+}
+
+Student.prototype.getAverage = function() {
+  return this.marks.reduce((acc, mark) => acc + mark, 0) / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
